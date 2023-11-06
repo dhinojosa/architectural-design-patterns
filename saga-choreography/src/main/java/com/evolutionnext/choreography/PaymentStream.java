50d09dea8a385d5db911419c732e65270be12f89
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2023-11-06 05:52:04 -0700

    Removed content for new adventures for now.

--- saga-choreography/src/main/java/com/evolutionnext/choreography/PaymentStream.java
+++
@@ -1,7 +0,0 @@
-package com.evolutionnext.choreography;
-
-public class PaymentStream {
-    public static void main(String[] args) {
-
-    }
-}
--- saga-choreography/src/main/java/com/evolutionnext/choreography/WarehouseStream.java
+++
@@ -1,7 +0,0 @@
-package com.evolutionnext.choreography;
-
-public class WarehouseStream {
-    public static void main(String[] args) {
-
-    }
-}
