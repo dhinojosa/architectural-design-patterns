220a5a7d0ef7b8cddbb7cd461fc1df051d63d516
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2024-10-02 09:54:52 -0600

    Changed version to 1.0.4

--- fussy-server/setup.sh
+++ fussy-server/setup.sh
@@ -2,8 +2,8 @@
 
 javac Main.java
 
-version=1.0.3
+version=1.0.4
 
 docker buildx create --use
-docker buildx build --platform linux/amd64,linux/arm64 -t dhinojosa/fussy-server:${version} .
+docker buildx build --platform linux/amd64,linux/arm64 -t dhinojosa/fussy-server:${version} . --push
 docker buildx build --load -t dhinojosa/fussy-server:${version} .
